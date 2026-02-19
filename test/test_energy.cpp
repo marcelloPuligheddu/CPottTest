35677420aa7e636075fea3b294293eb569941144
Marcello Puligheddu <33704456+marcelloPuligheddu@users.noreply.github.com>
2026-02-19 16:07:39 +0000

    Add unit test for energy conservation in CellularPotts

--- test/test_energy.cpp
+++ test/test_energy.cpp
@@ -1,8 +1,14 @@
+#include <gtest/gtest.h>
+#include <random>
+#include "CellularPotts.h"
+
 TEST(CPM, EnergyConservation) {
     std::mt19937 gen(1234);
     CellularPotts cpm(32, 10, gen);
+
     double E0 = cpm.total_energy();
     double dH = cpm.attempt_copy_once_and_return_dH();
     double E1 = cpm.total_energy();
+
     EXPECT_NEAR(E1 - E0, dH, 1e-12);
 }
