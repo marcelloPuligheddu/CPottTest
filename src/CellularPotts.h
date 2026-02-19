9c052c0d8380bf3b8dc2e2448de5f788b5de6785
Marcello Puligheddu <33704456+marcelloPuligheddu@users.noreply.github.com>
2026-02-19 16:54:43 +0000

    Refactor CellularPotts constructor and member variables

--- src/CellularPotts.h
+++ src/CellularPotts.h
@@ -5,25 +5,30 @@
 
 class CellularPotts {
 public:
-    CellularPotts(int L, int num_cells, std::mt19937 gen_);
+    CellularPotts(int L, int num_cells, std::mt19937& gen);
+
     void monte_carlo_step();
+    double compute_delta(int i, int j) const;
+    void apply_move(int i, int j);
+    bool accept(double dH);
+    bool attempt_copy();
     double total_energy() const;
     int total_volume() const;
+
+private:
     int L;
     std::vector<int> lattice;
     std::vector<int> volume;
     std::vector<int> V0;
     std::vector<std::vector<double>> J;
+
     double lambdaV;
     double temperature;
 
-    std::mt19937 gen;
+    std::mt19937& gen;
     std::uniform_int_distribution<int> dist_site;
     std::uniform_real_distribution<double> dist_prob;
 
-    inline int idx(int x, int y) const ;
-    int neighbor(int x, int y) ;
-    double adhesion_energy(int i, int new_sigma) ;
-    double volume_energy(int sigma_old, int sigma_new) ;
-    double attempt_copy() ;
+    int idx(int x, int y) const;
+    int random_neighbor(int x, int y);
 };
