c6d5937b95ef0ad356458adbd1ce568f4c670bb2
Marcello Puligheddu <33704456+marcelloPuligheddu@users.noreply.github.com>
2026-02-19 17:00:22 +0000

    Refactor CellularPotts class methods and structure

--- src/CellularPotts.cpp
+++ src/CellularPotts.cpp
@@ -14,8 +14,7 @@ CellularPotts :: CellularPotts(int L, int num_cells, std::mt19937 gen)
           temperature(10.0),
           gen(gen),
           dist_site(0, L-1),
-          dist_prob(0.0, 1.0)
-    {
+          dist_prob(0.0, 1.0) {
         // Initialize lattice with random cell IDs
         std::uniform_int_distribution<int> dist_cell(0, num_cells-1);
         for (int i = 0; i < L*L; ++i) {
@@ -25,27 +24,77 @@ CellularPotts :: CellularPotts(int L, int num_cells, std::mt19937 gen)
 
         // Example adhesion matrix
         J.resize(num_cells, std::vector<double>(num_cells, 16.0));
-    }
+}
 
-    void CellularPotts::monte_carlo_step() {
-        for (int k = 0; k < L*L; ++k)
-            attempt_copy();
-    }
+inline int CellularPotts::idx(int x, int y) const { return x + L*y; }
 
-    inline int CellularPotts::idx(int x, int y) const {
-        return x + L*y;
-    }
-
-    int CellularPotts::neighbor(int x, int y) {
+int CellularPotts::neighbor(int x, int y) {
         int dir = gen() % 4;
         if (dir == 0) x = (x+1) % L;
         if (dir == 1) x = (x-1+L) % L;
         if (dir == 2) y = (y+1) % L;
         if (dir == 3) y = (y-1+L) % L;
         return idx(x,y);
+}
+
+
+
+void CellularPotts::monte_carlo_step() { for (int k = 0; k < L*L; ++k) attempt_copy(); }
+
+double CellularPotts::compute_delta(int i, int j) const {
+    int sigma_old = lattice[i];
+    int sigma_new = lattice[j];
+
+    if (sigma_old == sigma_new) return 0.0;
+
+    double dH = 0.0;
+
+    // adhesion
+    int x = i % L;
+    int y = i / L;
+
+    int neigh[4] = {
+        idx((x+1)%L, y),
+        idx((x-1+L)%L, y),
+        idx(x, (y+1)%L),
+        idx(x, (y-1+L)%L)
+    };
+
+    for (int n : neigh) {
+        int sigma_n = lattice[n];
+        dH += J[sigma_new][sigma_n] * (sigma_new != sigma_n);
+        dH -= J[sigma_old][sigma_n] * (sigma_old != sigma_n);
     }
 
-    double CellularPotts::adhesion_energy(int i, int new_sigma) {
+    // volume
+    int V_old = volume[sigma_old];
+    int V_new = volume[sigma_new];
+
+    dH += lambdaV * (
+        (V_old-1 - V0[sigma_old])*(V_old-1 - V0[sigma_old])
+      - (V_old   - V0[sigma_old])*(V_old   - V0[sigma_old])
+    );
+
+    dH += lambdaV * (
+        (V_new+1 - V0[sigma_new])*(V_new+1 - V0[sigma_new])
+      - (V_new   - V0[sigma_new])*(V_new   - V0[sigma_new])
+    );
+
+    return dH;
+}
+
+void CellularPotts::apply_move(int i, int j)
+{
+    int sigma_old = lattice[i];
+    int sigma_new = lattice[j];
+
+    lattice[i] = sigma_new;
+    volume[sigma_old]--;
+    volume[sigma_new]++;
+}
+
+
+double CellularPotts::adhesion_energy(int i, int new_sigma) {
         int x = i % L;
         int y = i / L;
         double dH = 0.0;
@@ -68,9 +117,9 @@ CellularPotts :: CellularPotts(int L, int num_cells, std::mt19937 gen)
         }
 
         return dH;
-    }
+}
 
-    double CellularPotts::volume_energy(int sigma_old, int sigma_new) {
+double CellularPotts::volume_energy(int sigma_old, int sigma_new) {
         double dH = 0.0;
 
         // remove from old
@@ -84,34 +133,45 @@ CellularPotts :: CellularPotts(int L, int num_cells, std::mt19937 gen)
                        - (V_new - V0[sigma_new])*(V_new - V0[sigma_new]));
 
         return dH;
+}
+
+void CellularPotts::apply_move(int i, int j)
+{
+    int sigma_old = lattice[i];
+    int sigma_new = lattice[j];
+
+    lattice[i] = sigma_new;
+    volume[sigma_old]--;
+    volume[sigma_new]++;
+}
+
+bool CellularPotts::accept(double dH)
+{
+    if (dH <= 0.0)
+        return true;
+
+    return dist_prob(gen) < std::exp(-dH / temperature);
+}
+
+bool CellularPotts::attempt_copy()
+{
+    int x = dist_site(gen);
+    int y = dist_site(gen);
+
+    int i = idx(x,y);
+    int j = random_neighbor(x,y);
+
+    double dH = compute_delta(i, j);
+
+    if (accept(dH)) {
+        apply_move(i, j);
+        return true;
     }
 
-    double CellularPotts::attempt_copy() {
-        int x = dist_site(gen);
-        int y = dist_site(gen);
-        int i = idx(x,y);
+    return false;
+}
 
-        int j = neighbor(x,y);
-
-        int sigma_old = lattice[i];
-        int sigma_new = lattice[j];
-
-        if (sigma_old == sigma_new) return 0.0 ;
-
-        double dH = 0.0;
-        dH += adhesion_energy(i, sigma_new);
-        dH += volume_energy(sigma_old, sigma_new);
-
-        if (dH <= 0.0 || dist_prob(gen) < std::exp(-dH/temperature)) {
-            lattice[i] = sigma_new;
-            volume[sigma_old]--;
-            volume[sigma_new]++;
-        }
-        return dH;
-    }
-
-    double CellularPotts::total_energy() const
-    {
+double CellularPotts::total_energy() const {
         double H = 0.0;
     
         // --- Adhesion term ---
@@ -147,15 +207,13 @@ CellularPotts :: CellularPotts(int L, int num_cells, std::mt19937 gen)
             H += lambdaV * dv * dv;
         }
     
-        return H;
-    }
+        return H}
 
 
-    int CellularPotts::total_volume() const
-    {
+int CellularPotts::total_volume() const {
         int sum = 0;
         for (int v : volume)
             sum += v;
         return sum;
-    }
+}
 
