e6e6c8a9a46ed9e973e54cf53646e0ea7cd7002f
Marcello Puligheddu <33704456+marcelloPuligheddu@users.noreply.github.com>
2026-02-19 15:57:06 +0000

    Add test for volume conservation in CellularPotts

---
+++ test/test_volume.cpp
@@ -0,0 +1,9 @@
+TEST(CPM, VolumeConservation) {
+    std::mt19937 gen(1234);
+    CellularPotts cpm(32, 10, gen);
+
+    for (int i = 0; i < 1000; ++i)
+        cpm.monte_carlo_step();
+
+    EXPECT_EQ(cpm.total_volume(), 32*32);
+}
