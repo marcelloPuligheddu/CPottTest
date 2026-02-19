c75643f935b4939c9d9f61369f55393b4b3480ea
Marcello Puligheddu <33704456+marcelloPuligheddu@users.noreply.github.com>
2026-02-19 16:29:20 +0000

    Update energy calculation method in test

--- test/test_energy.cpp
+++ test/test_energy.cpp
@@ -7,7 +7,7 @@ TEST(CPM, EnergyConservation) {
     CellularPotts cpm(32, 10, gen);
 
     double E0 = cpm.total_energy();
-    double dH = cpm.attempt_copy_once_and_return_dH();
+    double dH = cpm.attempt_copy();
     double E1 = cpm.total_energy();
 
     EXPECT_NEAR(E1 - E0, dH, 1e-12);
