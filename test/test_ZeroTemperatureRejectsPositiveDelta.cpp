2a99cbb6c2303f9ea0f119a10f8ad30cc8bc8391
Marcello Puligheddu <33704456+marcelloPuligheddu@users.noreply.github.com>
2026-02-19 17:15:44 +0000

    Add test for zero temperature rejection of positive delta

---
+++ test/test_ZeroTemperatureRejectsPositiveDelta.cpp
@@ -0,0 +1,9 @@
+TEST(CPM, ZeroTemperatureRejectsPositiveDelta)
+{
+    std::mt19937 gen(1234);
+    CellularPotts cpm(16, 4, gen);
+    cpm.set_temperature(0.0);
+
+    EXPECT_FALSE(cpm.accept(5.0));
+    EXPECT_TRUE(cpm.accept(-1.0));
+}
