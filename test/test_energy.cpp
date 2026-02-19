874f0950784c0144725dc334fb0ec4eb26578b9a
Marcello Puligheddu <33704456+marcelloPuligheddu@users.noreply.github.com>
2026-02-19 17:17:45 +0000

    Refactor energy conservation test for clarity

--- test/test_energy.cpp
+++ test/test_energy.cpp
@@ -2,12 +2,18 @@
 #include <random>
 #include "CellularPotts.h"
 
-TEST(CPM, EnergyConservation) {
+TEST(CPM, DeltaMatchesFullEnergyDifference)
+{
     std::mt19937 gen(1234);
-    CellularPotts cpm(32, 10, gen);
+    CellularPotts cpm(16, 4, gen);
+
+    int i = 5;
+    int j = 6;
 
     double E0 = cpm.total_energy();
-    double dH = cpm.attempt_copy();
+    double dH = cpm.compute_delta(i, j);
+
+    cpm.apply_move(i, j);
     double E1 = cpm.total_energy();
 
     EXPECT_NEAR(E1 - E0, dH, 1e-12);
