f4c757f47864f4b9e9e1d402eb12a42ae17d89ad
Marcello Puligheddu <33704456+marcelloPuligheddu@users.noreply.github.com>
2026-02-19 17:18:19 +0000

    Adjust CellularPotts initialization parameters

--- test/test_volume.cpp
+++ test/test_volume.cpp
@@ -4,8 +4,11 @@
 
 TEST(CPM, ApplyMoveUpdatesVolume) {
     std::mt19937 gen(1234);
-    CellularPotts cpm(32, 10, gen);
+    CellularPotts cpm(16, 4, gen);
 
+    int i = 5;
+    int j = 6;    
+    
     int before = cpm.total_volume();
     cpm.apply_move(i, j);
     EXPECT_EQ(cpm.total_volume(), before);
