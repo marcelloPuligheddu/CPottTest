1d2bbea708f3bdd96cfe2df7fed67026866f6550
Marcello Puligheddu <33704456+marcelloPuligheddu@users.noreply.github.com>
2026-02-20 15:18:40 +0000

    Fix template instantiation for CellularPotts

--- test/test_ZeroTemperatureRejectsPositiveDelta.cpp
+++ test/test_ZeroTemperatureRejectsPositiveDelta.cpp
@@ -5,7 +5,7 @@
 TEST(CPM, ZeroTemperatureRejectsPositiveDelta)
 {
     std::mt19937 gen(1234);
-    CellularPotts cpm(16, 4, gen);
+    CellularPotts cpm<16, 4> (gen);
     cpm.set_temperature(0.0);
 
     EXPECT_FALSE(cpm.accept(5.0));
